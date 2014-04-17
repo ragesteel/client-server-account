409c2c4518acf43955d805b5bfcf0da9e7a23d5d
Igor Sakovich <ragesteel@gmail.com>
2014-04-17 23:32:17 +0400

    Добавление источника данных для Tomcat'а, добавление Listener'а от Liquibase. Добавлено чтение параметра для идентификатора. Дальше чтение параметра для значения, но это видимо уже после реализации какого-нибудь тестового клиента, чтобы на чём-то этот PUT-то проверить.

--- server/src/main/java/ru/javatalks/fundamentals/account/server/AccountServlet.java
+++ server/src/main/java/ru/javatalks/fundamentals/account/server/AccountServlet.java
@@ -1,5 +1,8 @@
 package ru.javatalks.fundamentals.account.server;
 
+import com.google.common.base.Optional;
+
+import javax.annotation.Nonnull;
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
 
-@WebServlet("/account")
+@WebServlet("/account/*")
 public class AccountServlet extends HttpServlet {
     @Resource(name = "jdbc/accountDB")
     private DataSource dataSource;
@@ -23,12 +26,12 @@ public class AccountServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Получить значения параметров
-        Integer id = 0;
-        req.getServletPath();
-        req.getContextPath();
+        Optional<Integer> optionalId = getNumberFrom(req, resp);
+        if (!optionalId.isPresent()) {
+            return;
+        }
         // Выполнить запрос
-        Long amount = accountServiceJdbc.getAmount(id);
+        Long amount = accountServiceJdbc.getAmount(optionalId.get());
         // Вернуть результат
         resp.getWriter().print(amount);
     }
@@ -36,10 +39,31 @@ public class AccountServlet extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Получить значения параметров
-        Integer id = 0;
+        Optional<Integer> optionalId = getNumberFrom(req, resp);
+        if (!optionalId.isPresent()) {
+            return;
+        }
         Long value = 0L;
         // Выполнить запрос
-        accountServiceJdbc.addAmount(id, value);
+        accountServiceJdbc.addAmount(optionalId.get(), value);
         // Вернуть результат
+    }
+
+    @Nonnull
+    private static Optional<Integer> getNumberFrom(@Nonnull HttpServletRequest req, @Nonnull HttpServletResponse resp)
+            throws IOException {
+        String pathInfo = req.getPathInfo();
+        if (null == pathInfo) {
+            resp.sendError(400, "Parameter not defined!");
+            return Optional.absent();
+        }
+        String parameter = pathInfo.substring(1);
+        Integer id;
+        try {
+            return Optional.of(Integer.valueOf(parameter));
+        } catch (NumberFormatException e) {
+            resp.sendError(400, "Unable to parse parameter: " + parameter);
+            return Optional.absent();
+        }
     }
 }
